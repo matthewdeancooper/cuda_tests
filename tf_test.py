a12491251c893a6a25a536e108a2a66b09942790
matthew <matthewdeancooper@gmail.com>
2021-02-19 11:30:27 +1100

    updated tf_test with print statements

--- tf_test.py
+++ tf_test.py
@@ -1,18 +1,24 @@
 import tensorflow as tf
 
+# Assert GPU exists
 physical_devices = tf.config.list_physical_devices('GPU')
+print(physical_devices)
 assert len(physical_devices) > 0
 
+# Dataset
 mnist = tf.keras.datasets.mnist
 
+# Load dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalise
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Test convolution network
 # Input shape (m, 28, 28) -> (m, 28, 28, 1)
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
+# Build a minimal CNN
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu',
         input_shape=(28, 28, 1)),
@@ -22,9 +28,13 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 model.compile(optimizer='adam',
-              loss=loss_fn,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=5)
 
+# Training
+history = model.fit(x_train, y_train, epochs=5)
+
+# Examine loss
+loss = history.history['loss']
+assert loss[0] > loss[-1]
